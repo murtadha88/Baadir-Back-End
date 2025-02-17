fe56078aa4f7927558c2b869614c090ecd687cec
salmanameen <salmanameen3138@gmail.com>
2025-02-17 20:33:55 +0300

    create event feature

--- controllers/baadir.js
+++ controllers/baadir.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const User = require('../models/User')
-
+const Event = require("../models/Event")
 const router = express.Router()
+const verifyToken = require("../middleware/verify-token")
 
-router.get("/applications", (req,res) => {
-    
+router.post("/event", verifyToken, async (req, res) => {
+    try {
+        req.body.userId = req.user._id;
+        const createEvent = await Event.create(req.body);
+        createEvent._doc.userId = req.user;
+        res.status(201).json(createEvent);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
+router.get("/", (req, res) => {
+    res.send("");
 })
-
-
-router.get("/", (req,res) => {
-    res.send("GG");
-})
-
 module.exports = router
