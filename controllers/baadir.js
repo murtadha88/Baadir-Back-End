f8d19beefb45be9f61bf502a1b43b267e9601a73
Murtadha Almoamen <murtadhaalmoamen8@gmail.com>
2025-02-17 20:33:43 +0300

    Completing the get request for the applications

--- controllers/baadir.js
+++ controllers/baadir.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const User = require('../models/User')
+const Application = require('../models/Application')
 
 const router = express.Router()
 
-router.get("/applications", (req,res) => {
-    
+router.get("/applications", async (req,res) => {
+    try {
+        const foundApplication = await Application.find()
+        res.status(200).json(foundApplication)
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
 })
-
 
 router.get("/", (req,res) => {
     res.send("GG");
